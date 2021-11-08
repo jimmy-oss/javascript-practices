493b807f3afa0f23023a93beacc99d96afd2598a
jimmy-oss <jimmynjonge51@gmail.com>
2021-11-08 16:07:52 +0300

    learnt about javascript operators

---
+++ js/operator.js
@@ -0,0 +1,33 @@
+//alert("hello world");
+// operator and operand 
+var nam1 = 22;
+nam1 = -nam1; // - is the unary operator and nam1 is operators,becoz  the - is working on only 1 variable called unary operator.
+console.log(nam1);
+
+ var num2 = 25;
+ var result  = 2 + num2; // + is the binary operator and nam2 is operator,becoz  the - is working on only 1 variable called binary operator.  *, -, + , /
+ console.log(result);
+
+ var x = 5;
+ var y = 2;
+
+ // Arithmetic Operation
+ console.log("result of the operation " + (x + y));
+ console.log("result of the subtraction " + (x - y));
+ console.log("result of the division " + (x / y));
+ console.log("result of the multiplication " + (x * y));
+ console.log("result of the exponent " + (x ** y)); // 25
+ console.log("result of the x++  is " + (x++)); // 5
+ console.log("result of the ++x  is " + (++x)); //  7
+
+ y = y ++; // y + 1
+ y+=1; // = y + 1
+ y-=1; // = y-1
+ y*=y //  = y * y
+
+ console.log("hello" + " - " + "world" );
+ var firstName = "jimmy";
+ var lastName = "wanyutu";
+ console.log("Your first name is " + firstName);
+ console.log("Your last name is " + lastName);
+ console.log("Hence your full name is "+  firstName + "-" + lastName);
