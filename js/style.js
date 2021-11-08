11739b95206ffddd9693bd65e5538ddf824651db
jimmy-oss <jimmynjonge51@gmail.com>
2021-11-08 14:11:17 +0300

    data types variables

---
+++ js/style.js
@@ -0,0 +1,73 @@
+/* Rules of Javascript */
+
+
+/* Declaring Variables */
+/*
+var myName = "kamau";
+var myAge = 23;
+console.log("my name is " + myName);
+console.log("my age is " + myAge);
+
+ 
+/* Declaring Variables In Alerts */
+/* var  myName = "jimmy";
+var myAge = 23;
+ alert("my name is:" + myName)
+alert("I am :" + myAge);
+*/
+
+
+
+ 
+ 
+ 
+ 
+ 
+ /* var num = 10;
+  console.log(num);
+
+  var myName = "jimmy";
+  console.log(myName);
+
+  var  isHere = true;
+  console.log(isHere);
+
+  var a = "a";
+  console.log(typeof a);
+  console.log(a);
+
+  a = null;
+  console.log(typeof a);
+  console.log(a);
+
+  var willChange = "changing";
+  console.log(typeof willChange);
+  console.log(willChange);
+  
+  willChange = 5;
+  console.log(typeof willChange);
+  console.log(willChange);
+
+  willChange = true;
+  console.log(typeof willChange);
+  console.log(willChange);
+*/
+// Operators practise
+ /* var  x =  3;
+  var y =  12; 
+ 
+  var result = x / y;
+  console.log(result);
+ */
+// var showName = prompt("What's your name?");
+ // alert("welcome "  +  showName);
+ // alert("Your name length is " + showName.length);
+
+ // var showName = 'you \'re name is "jimmy"';
+ // console.log(showName);
+/*var  age;
+age = prompt("what is your age?");
+console.log(typeof age);
+age = parseInt(age);
+console.log(typeof age);
+console.log("After 5 years you will be " + ( age + 5)); */
