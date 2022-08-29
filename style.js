8061b517069af88f591049ba65d21a839668e679
jimmy-oss <jimmynjonge51@gmail.com>
2022-08-29 16:04:45 +0300

    learnt variables and data types

--- style.js
+++ style.js
@@ -1,2 +1,17 @@
-let js = 'amazing';
-if (js = 'amazing') alert('javascript is fun!');
+// let js = 'amazing';
+// console.log(40+20+10-5);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "true";
+console.log(typeof javascriptIsFun);
+
+let isFun;
+console.log(isFun);
+
+isFun = 1992;
+console.log(typeof isFun);
+
