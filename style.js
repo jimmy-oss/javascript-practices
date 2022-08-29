badd41d5c1ea48f17920e0a9c64973ccd6b9725d
jimmy-oss <jimmynjonge51@gmail.com>
2022-08-29 16:17:58 +0300

    const rules in Javascript

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
 // let js = 'amazing';
 // console.log(40+20+10-5);
 
-let javascriptIsFun = true;
+/* let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
 console.log(typeof javascriptIsFun);
@@ -15,3 +15,17 @@ console.log(isFun);
 isFun = 1992;
 console.log(typeof isFun);
 
+*/
+// Var, Const and Let declarations
+
+let age = 35;
+age = 30;
+// is best used while mutating data.
+
+// Const is used in variables that are not supposed to change in the future.
+/* const myBirth = 1993;
+   myBirth = 1990;
+*/
+// In const there is an error becoz of the assigned variable
+// Inshort const creates a variable which we cannot reassign its immutable a variable
+// that cannot be mutated.
