b244052c4e4b1bc72d3e030eb90ffc1465d8c69a
jimmy-oss <jimmynjonge51@gmail.com>
2022-09-02 11:38:25 +0300

    learnt about comparison operators

--- style.js
+++ style.js
@@ -31,6 +31,7 @@ age = 30;
 // that cannot be mutated.
 // For good practise const and let are nice in writting clean codes.
 
+// This is a math operator
 const now = 2040;
 const myAge = now - 1998;
 const mySarah = now - 2018;
@@ -44,6 +45,7 @@ const lastName = 'Jammie';
 console.log(firstName + ' ' + lastName)
 // + operator is used for concatenating two strings into one inorder to add spaces in btw u add " "
 
+// This are assignment operators
 let x = 20 + 10 + 5;
 x += 20;// means (x = x + 20) which is = 55
 x *= 4; // means (x = x * 4) which is = 220
@@ -51,3 +53,9 @@ x++; // means (x = x + 1) which is 221
 x--; // means (x = x -1) which is 220
 console.log(x) // 35
 // Inshort let variable is able to mutate once the x operators is changed time by time into single scope unlike const there will be a naughty error.
+
+// Comparison operators gives a boolean output
+// its obvious sarah is 22 which means when comparing with greater than or less than in the console u will get a boolean output
+console.log(myAge > mySarah); // > , < , >=, <=
+console.log(mySarah >= 22) // >= means is sarah greater than 22 or equal to 22
+console.log(mySarah <= 35) // <= means is sarah less than or equal to 22
