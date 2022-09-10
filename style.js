2ec0432ad55239be0925e8283715bc0232437557
jimmy-oss <jimmynjonge51@gmail.com>
2022-09-10 16:17:51 +0300

    equality operators

--- style.js
+++ style.js
@@ -159,8 +159,24 @@ n = n - 1;
 console.log(n);
 */
 
+/*
 // 5 falsy values 0 , '', undefined, null, nan
 console.log(Boolean(0));
 console.log(Boolean(undefined));
 console.log(Boolean('jimmy'));
 console.log(Boolean({}));
+
+const money = 100;
+if(money){
+   console.log("Don't spend it all")
+}else{
+   console.log("You should get a job!")
+}
+let height;
+if(height){
+   console.log("height is defined")
+}else{
+   console.log("height is undefined")
+}
+*/
+// Equality operators
